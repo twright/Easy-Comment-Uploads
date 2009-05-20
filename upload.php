6da9e54a10fcbcbfb56aa0c85181af42a01f74f3
tomdwright <tomdwright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-20 16:32:19 +0000

    Start of i18n support, better upload (auto add) and option for image only support
    git-svn-id: http://plugins.svn.wordpress.org/easy-comment-uploads/trunk@119239 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- upload.php
+++ upload.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE HTML>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -9,6 +9,7 @@
         		$upload_dir = "./";
         		$fh = fopen($upload_dir . './upload_url.txt', 'r');
         		$upload_url = fread($fh, filesize('./upload_url.txt'));
+        		$only_images = false;
 
             if ($_FILES["file"]["error"] > 0){
               echo "Error code: " . $_FILES["file"]["error"];
@@ -18,21 +19,23 @@
                 echo "<b>" . $_FILES["file"]["name"] . "</b> already exists";
               } else if (eregi('image/', $_FILES['file']['type'])){
                 move_uploaded_file($_FILES["file"]["tmp_name"],
-                  $upload_dir . /* "/images/" . */
+                  $upload_dir .
                   $_FILES["file"]["name"]);
-                echo "<b>[img]</b>" . $upload_url /*. "images/" */
+                $upload_file = "<b>[img]</b>" . $upload_url
                   . $_FILES["file"]["name"] . "<b>[/img]</b>";
-              } else {
+              } else if (!$only_images) {
               	move_uploaded_file($_FILES["file"]["tmp_name"],
                   $upload_dir .
                   $_FILES["file"]["name"]);
-                echo "<b>[file]</b>" . $upload_url
+                $upload_file = "<b>[file]</b>" . $upload_url
                   . $_FILES["file"]["name"] . "<b>[/file]</b>";
-              }
+              } else {
+								_e('Sorry but your file could not be uploaded.');
+							}
             }
 
             echo "&nbsp;&nbsp;&nbsp; | ";
-            echo "<a href='./upload.html'>back</a>";
+            echo "<a href='./upload.html'>" . __('Upload more files') . "</a>";
         ?>
     </body>
 </html>
