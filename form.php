582678c9aa34d04cb202c06a2587d53e42fa0533
tomdwright <tomdwright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-28 00:58:26 +0000

    style tweaks for form
    git-svn-id: http://plugins.svn.wordpress.org/easy-comment-uploads/trunk@120985 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -1,16 +1,14 @@
 
 </form>
-<br />
 <!-- Easy comment uploads for Wordpress by Tom Wright: http://wordpress.org/extend/plugins/easy-comment-uploads/ -->
-<strong>Upload files:</strong>
-<p>You can include images or files in your comment by selecting them below. Once you select a file, it will be uploaded and a link to it added to your comment. You can upload as many images or files as you like and they will all be added to your comment.</p>
+<h3 style="clear: both">Upload files:</h3>
+<p style="margin-top: 4px">You can include images or files in your comment by selecting them below. Once you select a file, it will be uploaded and a link to it added to your comment. You can upload as many images or files as you like and they will all be added to your comment.</p>
 
 <form target="hiddenframe" enctype="multipart/form-data" action="<?php echo get_option('siteurl') . '/wp-content/plugins/easy-comment-uploads/' ?>upload.php" method="POST" name="uploadform" id="uploadform">
-<p>
+  <p style="text-align: center; margin-bottom: -15px">
   Select File:
-  <input type="file" name="file" id="fileField"   onchange="document.uploadform.submit()" />
-</p>
-<p id="uploadedfile" >
+  <input type="file" name="file" id="fileField"   onchange="document.uploadform.submit()" /></p>
+<p id="uploadedfile">
   <label></label>
 </p>
 <iframe name="hiddenframe" style="display:none" >Loading...</iframe>
